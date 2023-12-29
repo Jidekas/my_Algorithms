e3a66a9a33b3aae0968ee2ff6a3ae7ba49446938
Jidekas <132021739+Jidekas@users.noreply.github.com>
2023-12-29 12:42:57 +0100

    modified rangeCompact solutions

--- rangeCompact.js
+++ rangeCompact.js
@@ -1,57 +1,53 @@
 
-// const rangeCompact = nums => {
-//     let resArr = []
-//     let res2 = []
-//     let result = ''
-//     resArr.push([nums[0]])
+//solution1
+// function rangeCompact(nums){
+//   let resArr = []
+//   let result = []
+//   resArr.push(nums[0])
+
+//   //replace consecutive numbers with a '-'
+//   for(let i =1; i<nums.length; i++){
+//     if((nums[i]-nums[i-1] === 1) && (nums[i+1] - nums[i] === 1)){
+//       resArr.push('-')
+//     } else{
+//       resArr.push(nums[i])
+//     }
+//   }
   
-    
-//     //console.log(lastNum + 1)
-//     for(let i=1; i<nums.length; i++){   
-//     let lastArr = resArr[resArr.length -1]
-//     let lastNum = lastArr[lastArr.length -1]
-   
-      
-//       if(lastNum + 1 === nums[i]){
-//         lastArr.push(nums[i])
-//       }else{
-//         resArr.push([nums[i]])
+//   //filter out excess '-'
+//     let res = resArr.filter((a,i) => (a !== '-' || resArr[i+1] !== '-'))
+
+//   // assemble the resut structure in an array.  
+//     for(let i=0; i<res.length; i++){
+//       if(res[i] !== '-' && res[i-1] !== '-' && res[i+1] !== '-' ){
+//         result.push(`${res[i]},`)
+//       }
+
+//       if(res[i] === '-'){
+//         result.push(`${res[i-1]}-${res[i+1]},`)
 //       }
 //     }
-    
-//     for(i of resArr){
-      
-//       if(i.length > 2){
-//         let first = i[0]
-//         let last = i[i.length -1]
-//         let str = first + "-" + last
-//         res2.push(str)   
-//       } else{
-//         res2.push(...i)
-//       }  
-      
-//     }
-    
-  
-//     result = res2.join(',')
-    
-     
-//     return result;
-//   };
+
+//    //join the structured result array and return
+//     return result.join('')
+//   }
 
 
+
+//solution 2
 
 const rangeCompact = nums => {
+   //replace consecutive numbers with a '-'
     nums = nums.map((v, i) =>
         nums[i - 1] == v - 1 && nums[i + 1] == v + 1 ? "-" : v
     
       );
       console.log(nums)
+      //filter out excess '-'
       let res = nums.filter((v, i) => v != "-" || nums[i - 1] != "-"
       )
 
-      console.log(res)
-        
+      //join the filterd array with a ',' betwenn each character. & replace ,-, with -  
       const value = res.join(",").replace(/,-,/g, "-");
 
         return value
